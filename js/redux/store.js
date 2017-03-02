c1b6d292f5dc7895d285f7b82e2cdb0cba5e2bc8
Eric Snell <eric_snell21@yahoo.com>
2017-03-02 13:08:25 -0500

    ES6'd store.js

--- js/redux/store.js
+++ js/redux/store.js
@@ -1,9 +1,18 @@
-var redux = require('redux');
-//creates the store 
-var createStore = redux.createStore;
+import redux, { createStore } from 'redux'
+import reducers from './reducers'
 
-var reducers = require('./reducers');
+const store = createStore(reducers.gameReducer)
 
-var store = createStore(reducers.gameReducer);
+module.exports = store
 
-module.exports = store;
+
+// OLD CODE -- DELETE WHEN WORKING
+// var redux = require('redux');
+// //creates the store
+// var createStore = redux.createStore;
+//
+// var reducers = require('./reducers');
+//
+// var store = createStore(reducers.gameReducer);
+//
+// module.exports = store;
